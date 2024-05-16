44e6c9047b41ba54a6f6fdf5cfc916ac9c29954c
Alexy Grabov <alexy.grabov@cyberark.com>
2024-05-16 15:33:27 +0300

    add unit tests

--- aws_resource_validator/main.py
+++ aws_resource_validator/main.py
@@ -3,7 +3,7 @@ from aws_resource_validator.class_generator import generate_static_classes
 from aws_resource_validator.scrapper import fetch_and_parse_github
 
 # TODO: get token dynamically somehow...
-github_api_token: str = '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'  # your github api token
+github_api_token: str = ''  # your github api token
 headers: Dict[str, str] = {'Authorization': f'Bearer {github_api_token}'}
 
 if __name__ == "__main__":
