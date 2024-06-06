a926aa8d14bdf07833ed0505b9601041c0d20dd5
Alexy Grabov <58911092+CoreOxide@users.noreply.github.com>
2024-06-06 13:48:31 +0300

    Rename a bunch of stuff for productization  (#7)
    * add unit tests
    
    * add unit tests
    
    * rename some classes to be more user-friendly as part of productization efforts
    
    ---------
    
    Co-authored-by: Alexy Grabov <alexy.grabov@cyberark.com>

--- test_import.py
+++ test_import.py
@@ -1,10 +1,9 @@
-from aws_resource_validator.api_registry_definitions import Acm, api_registry
+from aws_resource_validator.class_definitions import Acm, class_registry
 
-# Now you can use `api_registry` with full class definitions
-acm: Acm = api_registry.Acm
-try:
-    print(acm.Arn.pattern)
-    print(acm.arn.validate("example-arn"))
-    print(acm.Arn.generate())
-except AttributeError as e:
-    print(e)
+# Use type hint so that you can use `api_registry` with full class definitions
+acm: Acm = class_registry.Acm
+
+print(acm.Arn.pattern)
+print(acm.Arn.type)
+print(acm.arn.validate("example-arn"))
+print(acm.Arn.generate())
