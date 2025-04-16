29044c09cd15bddf40f9960faaa7ca9c36bb5e80
Alexy Grabov <58911092+CoreOxide@users.noreply.github.com>
2025-04-16 21:21:08 +0300

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,8 +7,8 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
+    pkgs.python311
+    pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
     pkgs.poetry
