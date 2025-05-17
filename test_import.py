165c2fac8401674bfff28527543369a7c68a0149
Alexy Grabov <alexy.grabov@cyberark.com>
2025-05-17 11:33:42 +0300

    remove unused code

--- test_import.py
+++ test_import.py
@@ -1,4 +1,3 @@
-import boto3
 from aws_resource_validator.class_definitions import Acm, class_registry
 
 # Use type hint so that you can use `api_registry` with full class definitions
@@ -8,5 +7,3 @@ print(acm.Arn.pattern)
 print(acm.Arn.type)
 print(acm.arn.validate("example-arn"))
 print(acm.Arn.generate())
-
-boto3.resources
